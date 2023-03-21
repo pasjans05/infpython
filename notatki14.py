ac8e849f7dd7fa7baa2eb2e0fc9ab0c697d8d16a
Stanisław Kardaś <127191146+pasjans05@users.noreply.github.com>
2023-03-21 10:30:43 +0100

    notatki z lekcji 14.03.2023

--- notatki14.py
+++ notatki14.py
@@ -1,39 +1,39 @@
 # liczby parzyste:
 
-# i = 0
-# parz = 1
-# while (parz < 10):
-#     if parz % 2 == 0:
-#         i += 1
-#         print(f"liczba: {parz}")
-#     parz += 1
-# print(f"liczb parzystych: {i}")
+i = 0
+parz = 1
+while (parz < 10):
+    if parz % 2 == 0:
+        i += 1
+        print(f"liczba: {parz}")
+    parz += 1
+print(f"liczb parzystych: {i}")
 
 # funkcje:
 
-# def print_name(first_name, last_name, age = 18):
-#     print(f"Twoje imie to: {first_name} {last_name} of age: {age}")
-#
-# print_name("asdsadf", "sssss")
+def print_name(first_name, last_name, age = 18):
+    print(f"Twoje imie to: {first_name} {last_name} of age: {age}")
+
+print_name("asdsadf", "sssss")
 
 # funkcje 2:
 
-# def greet(name):
-#     print(f"Hi {name}")
-#     return True
-# print(greet("Staszek"))
+def greet(name):
+    print(f"Hi {name}")
+    return True
+print(greet("Staszek"))
 
 # krotka:
 
-# def multiply(*numbers):
-#     print(type(numbers))
-# multiply(2, 3, 4, 5)
+def multiply(*numbers):
+    print(type(numbers))
+multiply(2, 3, 4, 5)
 
 # słownik:
 
-# def save_user(**user):
-#     print (user)
-# save_user(id = 2, name = "Filip")
+def save_user(**user):
+    print (user)
+save_user(id = 2, name = "Filip")
 
 # multiply:
 
